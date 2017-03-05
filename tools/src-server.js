515953e69b9313c177204c38c0585e97c1e89f6f
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-03-05 12:50:24 +0100

    Added webpack configuration including hot reloading. Added initial testing configurations.

--- tools/src-server.js
+++ tools/src-server.js
@@ -11,6 +11,11 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 import webpack from 'webpack';
+import path from 'path';
 
 export default {
-  debug: true,
-  noInfo: false,
+  devtool: 'inline-source-map',
   entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
     path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
@@ -15,17 +17,18 @@ export default {
   devServer: {
     contentBase: path.resolve(__dirname, 'src')
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']}
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
-  },
-  externals: {
-    // Include createjs script on your page, then add the below.
-    // The left hand side represents the global module that gets exposed to your ES6 code
-    // The right hand side represents the object that is exposed/imported from your externally referenced script.
-
-    "createjs": "createjs"
   }
 };
