394b64a1eae17de0482d6c17265ac113f99f56e1
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-03-12 18:08:56 +0100

    Generate a given number of balls on the stage.

--- index.js
+++ index.js
@@ -6,22 +6,32 @@ import createjs from 'createjs'
 export default class App {
   constructor() {
     console.log('>>> App initialized...')
+
+    this.BALLS_COUNT = 75
+
     this.init()
   }
 
   init() {
-    let stage = new createjs.Stage('app');
-    stage.canvas.width = 800;
-    stage.canvas.height = 600;
+    let stage = new createjs.Stage('app')
+    stage.canvas.width = 800
+    stage.canvas.height = 600
 
-    let bitmap = new createjs.Bitmap('static/images/ball7b.png');
-    bitmap.x = 100;
-    bitmap.y = 100;
-    bitmap.shadow = new createjs.Shadow("#000000", 5, 5, 10);
-    stage.addChild(bitmap);
+    let i = 0
+    while(i < this.BALLS_COUNT) {
+      let bitmap = new createjs.Bitmap('static/images/ball7b.png')
 
-    createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener('tick', stage);
+      bitmap.x = Math.floor(Math.random() * stage.canvas.width)
+      bitmap.y = Math.floor(Math.random() * stage.canvas.height)
+
+      console.log('coords:', bitmap.x, bitmap.y)
+      bitmap.shadow = new createjs.Shadow("#000000", 5, 5, 10)
+      stage.addChild(bitmap);
+      i++;
+    }
+
+    createjs.Ticker.setFPS(60)
+    createjs.Ticker.addEventListener('tick', stage)
   }
 }
 
