8e7cd651591752f4f7fe548512dfc74420cc570b
mihailgaberov <$Vale@meama010>
2020-06-24 09:00:09 +0300

    :lipstick: Syntax refurbishing.

--- src/physics.js
+++ src/physics.js
@@ -1,6 +1,7 @@
 const width = 500
 const height = 400
-let canvas = ctx = false
+let canvas = false
+let ctx = false
 const frameRate = 1 / 40 // Seconds
 const frameDelay = frameRate * 1000 // ms
 let loopTimer = false
@@ -30,102 +31,63 @@ const Cd = 0.47  // Dimensionless
 const rho = 1.22 // kg / m^3
 const A = Math.PI * ball.radius * ball.radius / (10000) // m^2
 const ag = 9.81  // m / s^2
-const mouse = { x: 0, y: 0, isDown: false }
-
-const getMousePosition = (e) => {
-  mouse.x = e.pageX - canvas.offsetLeft;
-  mouse.y = e.pageY - canvas.offsetTop;
-}
-
-const mouseDown = (e) => {
-  if (e.which === 1) {
-    getMousePosition(e)
-    mouse.isDown = true
-    ball.position.x = mouse.x
-    ball.position.y = mouse.y
-  }
-}
-
-const mouseUp = (e) => {
-  if (e.which === 1) {
-    mouse.isDown = false
-    ball.velocity.y = (ball.position.y - mouse.y) / 10
-    ball.velocity.x = (ball.position.x - mouse.x) / 10
-  }
-}
 
 const setup = () => {
   canvas = document.getElementById('canvas')
   ctx = canvas.getContext('2d')
-
-  canvas.onmousemove = getMousePosition
-  canvas.onmousedown = mouseDown
-  canvas.onmouseup = mouseUp
-
   ctx.fillStyle = 'red'
   ctx.strokeStyle = '#000000'
   loopTimer = setInterval(loop, frameDelay)
 }
 
 const loop = () => {
-  if (!mouse.isDown) {
-    // Do physics
-    // Drag force: Fd = -1/2 * Cd * A * rho * v * v
-    let Fx = -0.5 * Cd * A * rho * ball.velocity.x * ball.velocity.x * ball.velocity.x / Math.abs(ball.velocity.x)
-    let Fy = -0.5 * Cd * A * rho * ball.velocity.y * ball.velocity.y * ball.velocity.y / Math.abs(ball.velocity.y)
+  // Do physics
+  // Drag force: Fd = -1/2 * Cd * A * rho * v * v
+  let Fx = -0.5 * Cd * A * rho * ball.velocity.x * ball.velocity.x * ball.velocity.x / Math.abs(ball.velocity.x)
+  let Fy = -0.5 * Cd * A * rho * ball.velocity.y * ball.velocity.y * ball.velocity.y / Math.abs(ball.velocity.y)
 
-    Fx = (isNaN(Fx) ? 0 : Fx);
-    Fy = (isNaN(Fy) ? 0 : Fy);
+  Fx = (isNaN(Fx) ? 0 : Fx)
+  Fy = (isNaN(Fy) ? 0 : Fy)
 
-    // Calculate acceleration ( F = ma )
-    const ax = Fx / ball.mass
-    const ay = ag + (Fy / ball.mass)
-    // Integrate to get velocity
-    ball.velocity.x += ax*frameRate;
-    ball.velocity.y += ay*frameRate;
+  // Calculate acceleration ( F = ma )
+  const ax = Fx / ball.mass
+  const ay = ag + (Fy / ball.mass)
+  // Integrate to get velocity
+  ball.velocity.x += ax * frameRate
+  ball.velocity.y += ay * frameRate
 
-    // Integrate to get position
-    ball.position.x += ball.velocity.x*frameRate*100;
-    ball.position.y += ball.velocity.y*frameRate*100;
-  }
+  // Integrate to get position
+  ball.position.x += ball.velocity.x * frameRate * 100
+  ball.position.y += ball.velocity.y * frameRate * 100
+
   // Handle collisions
   if (ball.position.y > height - ball.radius) {
-    ball.velocity.y *= ball.restitution;
-    ball.position.y = height - ball.radius;
+    ball.velocity.y *= ball.restitution
+    ball.position.y = height - ball.radius
   }
   if (ball.position.x > width - ball.radius) {
-    ball.velocity.x *= ball.restitution;
-    ball.position.x = width - ball.radius;
+    ball.velocity.x *= ball.restitution
+    ball.position.x = width - ball.radius
   }
   if (ball.position.x < ball.radius) {
-    ball.velocity.x *= ball.restitution;
-    ball.position.x = ball.radius;
+    ball.velocity.x *= ball.restitution
+    ball.position.x = ball.radius
   }
-
 
   // Draw the ball
-  ctx.clearRect(0,0,width,height);
-  ctx.save();
+  ctx.clearRect(0, 0, width, height)
+  ctx.save()
 
-  ctx.translate(ball.position.x, ball.position.y);
-  ctx.beginPath();
-  ctx.arc(0, 0, ball.radius, 0, Math.PI*2, true);
-  ctx.closePath();
-  ctx.fill();
-  ctx.fillStyle = "white";
-  ctx.textAlign = "center";
-  ctx.font = "bold " + ball.radius + "px serif";
-  ctx.fillText('7', 0, 5);
-  ctx.restore();
-
-  // Draw the slingshot
-  if (mouse.isDown) {
-    ctx.beginPath();
-    ctx.moveTo(ball.position.x, ball.position.y);
-    ctx.lineTo(mouse.x, mouse.y);
-    ctx.stroke();
-    ctx.closePath();
-  }
+  ctx.translate(ball.position.x, ball.position.y)
+  ctx.beginPath()
+  ctx.arc(0, 0, ball.radius, 0, Math.PI * 2, true)
+  ctx.closePath()
+  ctx.fill()
+  ctx.fillStyle = 'white'
+  ctx.textAlign = 'center'
+  ctx.font = 'bold ' + ball.radius + 'px serif'
+  ctx.fillText('7', 0, 5)
+  ctx.restore()
 }
 
-setup();
+setup()
