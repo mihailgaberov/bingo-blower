47e70ebce5f7a655ba9315f64f9b48d8a4eeb95b
mihailgaberov <$Vale@meama010>
2020-06-26 22:54:44 +0300

    :construction: Playing with MatterJS.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-/**
- * Created by Mihail on 3/5/2017.
- */
-import createjs from 'createjs'
-import Ball from './src/Ball'
+// import createjs from 'createjs'
+// import Ball from './src/Ball'
+import Matter from 'matter-js';
 
 export default class App {
   constructor() {
@@ -10,10 +8,12 @@ export default class App {
 
     this.BALLS_COUNT = 75
 
-    this.init()
+    this.initMatter();
+
+    // this.init()
   }
 
-  init() {
+  /*init() {
     let stage = new createjs.Stage('app')
     stage.canvas.width = 800
     stage.canvas.height = 600
@@ -27,6 +27,35 @@ export default class App {
 
     createjs.Ticker.setFPS(60)
     createjs.Ticker.addEventListener('tick', stage)
+  }*/
+
+  initMatter() {
+    const Engine = Matter.Engine,
+      Render = Matter.Render,
+      World = Matter.World,
+      Bodies = Matter.Bodies
+
+    const engine = Engine.create()
+
+    const render = Render.create({
+      element: document.body,
+      engine: engine,
+      options: {
+        width: 800,
+        height: 400,
+        wireframes: false
+      }
+    })
+
+    const boxA = Bodies.rectangle(400, 200, 80, 80)
+    const ballA = Bodies.circle(380, 100, 40, 10)
+    const ballB = Bodies.circle(460, 10, 40, 10)
+    const ground = Bodies.rectangle(400, 380, 810, 60, { isStatic: true })
+
+    World.add(engine.world, [boxA, ballA, ballB, ground]);
+
+    Engine.run(engine);
+    Render.run(render);
   }
 }
 
