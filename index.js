781cef40636fa10134ee2ea0444e5d80fb4b24ec
mihailgaberov <$Vale@meama010>
2020-06-28 14:42:35 +0300

    :sparkles: Finished making a lotto blower effect.

--- index.js
+++ index.js
@@ -2,34 +2,10 @@
 // import Ball from './src/Ball'
 import Matter from 'matter-js'
 
+const BALLS_COUNT = 45
+
 export default class App {
-  constructor () {
-    console.log('>>> App initialized...')
-
-    this.BALLS_COUNT = 75
-
-    this.initMatter()
-
-    // this.init()
-  }
-
-  /*init() {
-    let stage = new createjs.Stage('app')
-    stage.canvas.width = 800
-    stage.canvas.height = 600
-
-    let i = 0
-    while(i < this.BALLS_COUNT) {
-      const ball = new Ball(stage.canvas.width, stage.canvas.height)
-      stage.addChild(ball.bitmap)
-      i++;
-    }
-
-    createjs.Ticker.setFPS(60)
-    createjs.Ticker.addEventListener('tick', stage)
-  }*/
-
-  initMatter () {
+  static init () {
     const Engine = Matter.Engine,
       Render = Matter.Render,
       World = Matter.World,
@@ -49,38 +25,52 @@ export default class App {
         width: 500,
         height: 500,
         background: 'transparent'
-        // background: 'static/images/blower.png'
       }
     })
-    const ballC = Bodies.circle(render.canvas.width / 2, 10, 20, {
-      restitution: 1,
-      render: {
-        sprite: {
-          texture: 'static/images/ball7b.png'
-        }
-      }
-    })
-    const onRenderTick = () => {
-      console.log('y:', ballC.position.y);
-      console.log('h:', render.canvas.height);
-      if (ballC.position.y >= render.canvas.height - 60) {
-        Body.applyForce(ballC, { x: ballC.position.x, y: ballC.position.y }, { x: .005, y: -0.05 })
-      }
 
-      if (ballC.position.y < ballC.circleRadius) {
-        Body.applyForce(ballC, { x: ballC.position.x, y: ballC.position.y }, { x: -0.005, y: 0.05 })
-      }
+    const balls = []
 
-      if (ballC.position.x < ballC.circleRadius) {
-        Body.applyForce(ballC, { x: ballC.position.x, y: ballC.position.y }, { x: 0.05, y: -0.005 })
-      }
-      if (ballC.position.x > render.canvas.width - 60) {
-        Body.applyForce(ballC, { x: ballC.position.x, y: ballC.position.y }, { x: -0.05, y: 0.005 })
-      }
+    const createBall = () => {
+      const ball = Bodies.circle(
+        render.canvas.width / 2,
+        render.canvas.height / 2,
+        20, {
+          restitution: 1,
+          render: {
+            sprite: {
+              texture: 'static/images/ball7b.png'
+            }
+          }
+        })
+      balls.push(ball)
+      return ball
     }
-    Events.on(runner, 'tick', onRenderTick)
-    World.add(engine.world, [ballC])
 
+    const onRenderTick = () => {
+      balls.forEach(ball => {
+        if (ball.position.y >= render.canvas.height - 60) {
+          Body.applyForce(ball, { x: ball.position.x, y: ball.position.y }, { x: .005, y: -0.07 })
+        }
+        if (ball.position.y < ball.circleRadius) {
+          Body.applyForce(ball, { x: ball.position.x, y: ball.position.y }, { x: -0.005, y: 0.05 })
+        }
+
+        if (ball.position.x < ball.circleRadius) {
+          Body.applyForce(ball, { x: ball.position.x, y: ball.position.y }, { x: 0.05, y: -0.005 })
+        }
+
+        if (ball.position.x > render.canvas.width - 60) {
+          Body.applyForce(ball, { x: ball.position.x, y: ball.position.y }, { x: -0.05, y: 0.005 })
+        }
+      })
+    }
+
+    // Add the balls to the scene
+    for (let i = 0; i < BALLS_COUNT; i++) {
+      World.add(engine.world, createBall())
+    }
+
+    // Run the engine
     Engine.run(engine)
     Render.run(render)
 
@@ -97,20 +87,20 @@ export default class App {
       return body
     }
 
-    const sW = 500;
-    const sH = 500;
-    const m = Math.min(sW, sH);
-    const rat = 1 / 4.5 * 2;
-    const r = m * rat;
-    const pegCount = 64;
-    const TAU = Math.PI * 2;
+    const sW = 500
+    const sH = 500
+    const m = Math.min(sW, sH)
+    const rat = 1 / 4.5 * 2
+    const r = m * rat
+    const pegCount = 64
+    const TAU = Math.PI * 2
     for (let i = 0; i < pegCount; i++) {
-      const segment = TAU / pegCount;
-      const angle2 = i / pegCount * TAU + segment / 2;
-      const x2 = Math.cos(angle2);
-      const y2 = Math.sin(angle2);
-      const cx2 = x2 * r + sW / 2;
-      const cy2 = y2 * r + sH / 2;
+      const segment = TAU / pegCount
+      const angle2 = i / pegCount * TAU + segment / 2
+      const x2 = Math.cos(angle2)
+      const y2 = Math.sin(angle2)
+      const cx2 = x2 * r + sW / 2
+      const cy2 = y2 * r + sH / 2
       addRect({
         x: cx2, y: cy2, w: 10 / 1000 * m, h: 300 / 1000 * m, options: {
           angle: angle2, isStatic: true, density: 1, render: {
@@ -119,10 +109,14 @@ export default class App {
             lineWidth: 0
           }
         }
-      });
+      })
     }
     // Build the circle bounds - END
+
+    // Start the blowing with X seconds delay
+    setTimeout(() => {
+      Events.on(runner, 'tick', onRenderTick)
+    }, 3000);
   }
 }
-
-const app = new App()
+App.init()
